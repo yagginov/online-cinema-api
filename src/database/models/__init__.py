890a5752ec21fa5ec3085d355889b58471ef8d83
Bohdan Yagginov <bhders62@gmail.com>
2025-10-13 14:07:50 +0300

    feature: created movie models

--- src/database/models/__init__.py
+++ src/database/models/__init__.py
@@ -1,1 +1,12 @@
 from .base import Base
+from .movies import (
+    GUID,
+    CertificationModel,
+    DirectorModel,
+    DirectorsMoviesModel,
+    GenreModel,
+    MovieModel,
+    MoviesGenresModel,
+    StarModel,
+    StarsMoviesModel,
+)
