c04c34330a8c8185b5ed926587a28d0fd85595c0
Serhii Basok <sergeybsk19@gmail.com>
2025-10-13 16:27:28 +0300

    Add user creation factory, password management, and email validation to User model

--- src/database/models/interactions.py
+++ src/database/models/interactions.py
@@ -7,10 +7,11 @@ from datetime import datetime, date, timedelta, timezone
 from sqlalchemy.orm import (
     Mapped,
     mapped_column,
-    relationship
+    relationship, validates
 )
 
-from src.database import Base
+from src.database import Base, validators
+from src.security.passwords import hash_password, verify_password
 from src.security.utils import generate_secure_token
 
 
@@ -79,6 +80,43 @@ class  User(Base):
         back_populates="user",
         cascade="all, delete-orphan"
     )
+
+    def has_group(self, group_name: UserGroupEnum) -> bool:
+        return self.group.name == group_name
+
+    @classmethod
+    def create(cls, email: str, raw_password: str, group_id: int | Mapped[int]) -> "User":
+        """
+        Factory method to create a new UserModel instance.
+
+        This method simplifies the creation of a new user by handling
+        password hashing and setting required attributes.
+        """
+        user = cls(email=email, group_id=group_id)
+        user.password = raw_password
+        return user
+
+    @property
+    def password(self) -> None:
+        raise AttributeError("Password is write-only. Use the setter to set the password.")
+
+    @password.setter
+    def password(self, raw_password: str) -> None:
+        """
+        Set the user's password after validating its strength and hashing it.
+        """
+        validators.validate_password_strength(raw_password)
+        self.hashed_password = hash_password(raw_password)
+
+    def verify_password(self, raw_password: str) -> bool:
+        """
+        Verify the provided password against the stored hashed password.
+        """
+        return verify_password(raw_password, self.hashed_password)
+
+    @validates("email")
+    def validate_email(self, key, value):
+        return validators.validate_email(value.lower())
 
 
 class UserProfile(Base):
