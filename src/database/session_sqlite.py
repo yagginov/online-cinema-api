4c06610a8ae953b0e8eae36e2c9762a5ab7773cf
Bohdan Yagginov <bhders62@gmail.com>
2025-10-13 16:16:51 +0300

    refactor: changed to pass all github actions

--- src/database/session_sqlite.py
+++ src/database/session_sqlite.py
@@ -5,6 +5,7 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
 from config import get_settings
+
 from .models import Base
 
 settings = get_settings()
