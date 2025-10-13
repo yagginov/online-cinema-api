a09e3e3fd2c1ffdd728574f16ea7d4ddb77cacc0
MorsImmortalis05 <morsimmortalis05@gmail.com>
2025-10-13 16:59:58 +0300

    implemented get_Cart endpoint

---
+++ src/schemas/carts.py
@@ -0,0 +1,17 @@
+from datetime import date, datetime
+from typing import Optional, List
+
+from pydantic import BaseModel, Field, field_validator
+
+from database.models import MovieModel
+
+
+class CartRequestSchema(BaseModel):
+    cart_id: int
+
+class CartResponseSchema(BaseModel):
+    id: int
+    movies: list[MovieModel]
+
+    class Config:
+        orm_mode = True
