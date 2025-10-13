d59ad6e142a65b5602c2e1a3d37b963b16f31f52
MorsImmortalis05 <morsimmortalis05@gmail.com>
2025-10-13 15:29:01 +0300

    started cartitem and cart models

---
+++ src/database/models/cart.py
@@ -0,0 +1,55 @@
+import datetime
+
+from sqlalchemy import (
+    ForeignKey,
+    func,
+    DateTime, UniqueConstraint,
+)
+from sqlalchemy.orm import mapped_column, Mapped, relationship
+
+from database import Base
+
+
+class CartModel(Base):
+    __tablename__ = "carts"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id",
+        ondelete="CASCADE"), nullable=False, unique=True
+    )
+    user = relationship("UserModel", back_populates="cart")
+    items: Mapped[list["CartItem"]] = relationship(
+        "CartItem",
+        back_populates="cart",
+        cascade="all, delete-orphan",
+    )
+
+    def __repr__(self):
+        return f"<cart id: {self.id}, user id: {self.user_id}>"
+
+
+
+class CartItem(Base):
+    __tablename__ = "cart_items"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    cart_id: Mapped[int] = mapped_column(
+        ForeignKey("carts.id",
+        ondelete="CASCADE"), nullable=False
+        )
+    cart = relationship("CartModel", back_populates="items")
+    movie_id: Mapped[int] = mapped_column(
+        ForeignKey("movies.id", ondelete="CASCADE"),
+        nullable=False,
+    )
+    added_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
+
+    __table_args__ = (
+        UniqueConstraint("cart_id", "movie_id", name="uq_cart_movie"),
+    )
+
+    def __repr__(self):
+        return f"<item id: {self.id}, cart id: {self.cart_id}>"
