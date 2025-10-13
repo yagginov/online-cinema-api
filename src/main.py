e5f56c681edcb4a65dc0af405a76cc513e47959e
Bohdan Yagginov <bhders62@gmail.com>
2025-10-13 20:18:38 +0300

    feature: create movie router

--- src/main.py
+++ src/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 
+from routes import movie_router
+
 app = FastAPI()
 
+API_VERSION_PREFIX = "/api/v1"
 
 @app.get("/")
 async def get_index():
     return {"message": "Hello World"}
+
+
+
+app.include_router(movie_router, prefix=f"{API_VERSION_PREFIX}/cinema", tags=["cinema"])
