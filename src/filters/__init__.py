e664b1c1d35daa62b74e933ebab2d1b3588d4fe8
Oleksandr Popov <orbik.rumion22@gmail.com>
2025-10-13 21:09:40 +0300

    feature: created filters module with base.py and movie_filters.py

---
+++ src/filters/__init__.py
@@ -0,0 +1,10 @@
+from .base import BasePaginationParams, BaseSortParams, SortOrderEnum
+from .movie_filters import MovieFilterParams, MovieSortByEnum
+
+__all__ = [
+    "BasePaginationParams",
+    "BaseSortParams",
+    "SortOrderEnum",
+    "MovieFilterParams",
+    "MovieSortByEnum",
+]
