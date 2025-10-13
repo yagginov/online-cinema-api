f515aef440195f63a13019f1a6f9375f1c960883
Serhii Basok <sergeybsk19@gmail.com>
2025-10-13 14:06:34 +0300

    Add UserGroup model to interactions

--- src/database/models/interactions.py
+++ src/database/models/interactions.py
@@ -1,5 +1,13 @@
 import enum
-from sqlalchemy import Enum
+from sqlalchemy import Enum, Column, Integer
+from sqlalchemy.orm import (
+    Mapped,
+    mapped_column,
+    relationship,
+    validates
+)
+
+from src.database import Base
 
 
 class UserGroupEnum(str, enum.Enum):
@@ -11,3 +19,14 @@ class UserGroupEnum(str, enum.Enum):
 class GenderEnum(str, enum.Enum):
     MAN = "man"
     WOMAN = "woman"
+
+
+class UserGroup(Base):
+    __tablename__ = "user_groups table"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    name: Mapped[UserGroupEnum] = mapped_column(Enum(UserGroupEnum), nullable=False, unique=True)
+
+    def __repr__(self):
+        return f"<UserGroupModel(id={self.id}, name={self.name})>"
+
