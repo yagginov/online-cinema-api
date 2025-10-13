737ce69891fadd98482287ebd619cc691bd24c62
Serhii Basok <sergeybsk19@gmail.com>
2025-10-13 14:35:06 +0300

    Add User model and relationships to interactions

--- src/database/models/interactions.py
+++ src/database/models/interactions.py
@@ -1,5 +1,8 @@
 import enum
-from sqlalchemy import Enum, Column, Integer
+from typing import List
+
+from sqlalchemy import Enum, Column, Integer, String, Boolean, ForeignKey, DateTime, func
+from datetime import datetime, date, timedelta, timezone
 from sqlalchemy.orm import (
     Mapped,
     mapped_column,
@@ -22,11 +25,33 @@ class GenderEnum(str, enum.Enum):
 
 
 class UserGroup(Base):
-    __tablename__ = "user_groups table"
+    __tablename__ = "user_groups"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     name: Mapped[UserGroupEnum] = mapped_column(Enum(UserGroupEnum), nullable=False, unique=True)
 
+    users: Mapped[List["User"]] = relationship("User", back_populates="group")
+
     def __repr__(self):
         return f"<UserGroupModel(id={self.id}, name={self.name})>"
 
+
+class  User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False, index=True)
+    hashed_password: Mapped[str] = mapped_column("hashed_password", String(255), nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False
+    )
+    group_id: Mapped[int] = mapped_column(ForeignKey("user_groups.id"), ondelete="CASCADE", nullable=False)
+    group: Mapped["UserGroup"] = relationship("UserGroup", back_populates="users")
+    profile: Mapped["UserProfile"] = relationship("UserProfile", back_populates="user", uselist=False)
+
+    def __repr__(self):
+        return f"<UserModel(id={self.id}, email={self.email}, is_active={self.is_active})>"
