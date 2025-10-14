c8afe57dddfa670c516dfc9535674d7b08a202cc
Bohdan Yagginov <bhders62@gmail.com>
2025-10-14 20:39:38 +0300

    feature: create schemas for movie app

---
+++ src/schemas/movies/__init__.py
@@ -0,0 +1,11 @@
+from .certifications import CertificationSchema
+from .directors import DirectorSchema
+from .genres import GenreSchema
+from .movies import (
+    MovieCreateRequestSchema,
+    MovieDetailResponseSchema,
+    MovieListItemSchema,
+    MoviePaginatedResponseSchema,
+    MovieUpdateRequestSchema,
+)
+from .stars import StarSchema
---
+++ src/schemas/movies/genres.py
@@ -0,0 +1,7 @@
+from pydantic import BaseModel, ConfigDict
+
+
+class GenreSchema(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    id: int
+    name: str
