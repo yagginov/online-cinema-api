ce92fdbc3072b938056fc8c499b51c8fd554f9b9
Serhii Basok <sergeybsk19@gmail.com>
2025-10-14 10:45:53 +0300

    Refactor accounts model: rename `hashed_password` to `_hashed_password` and adjust `group_id` with `ondelete="CASCADE"`

--- src/database/models/accounts.py
+++ src/database/models/accounts.py
@@ -50,13 +50,13 @@ class User(Base):
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False, index=True)
-    hashed_password: Mapped[str] = mapped_column("hashed_password", String(255), nullable=False)
+    _hashed_password: Mapped[str] = mapped_column("hashed_password", String(255), nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
     updated_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False
     )
-    group_id: Mapped[int] = mapped_column(ForeignKey("user_groups.id"), nullable=False)
+    group_id: Mapped[int] = mapped_column(ForeignKey("user_groups.id", ondelete="CASCADE"), nullable=False)
     group: Mapped["UserGroup"] = relationship("UserGroup", cascade="all, delete", back_populates="users")
 
     def __repr__(self):
