d7ed0608255b6c5122b3a712e13b5c4deb2bce9c
Serhii Basok <sergeybsk19@gmail.com>
2025-10-14 15:57:03 +0300

    feature: add email configuration and template constants to settings

--- src/config/settings.py
+++ src/config/settings.py
@@ -10,6 +10,21 @@ class BaseAppSettings(BaseSettings):
     PATH_TO_DB: str = str(BASE_DIR / "database" / "source" / "cinema.db")
     PATH_TO_MOVIES_CSV: str = str(BASE_DIR / "database" / "seed_data" / "imdb_movies.csv")
 
+    PATH_TO_EMAIL_TEMPLATES_DIR: str = str(BASE_DIR / "notifications" / "templates")
+    ACTIVATION_EMAIL_TEMPLATE_NAME: str = "activation_request.html"
+    ACTIVATION_COMPLETE_EMAIL_TEMPLATE_NAME: str = "activation_complete.html"
+    PASSWORD_RESET_TEMPLATE_NAME: str = "password_reset_request.html"
+    PASSWORD_RESET_COMPLETE_TEMPLATE_NAME: str = "password_reset_complete.html"
+
+    LOGIN_TIME_DAYS: int = 7
+
+    EMAIL_HOST: str = os.getenv("EMAIL_HOST", "host")
+    EMAIL_PORT: int = int(os.getenv("EMAIL_PORT", 25))
+    EMAIL_HOST_USER: str = os.getenv("EMAIL_HOST_USER", "testuser")
+    EMAIL_HOST_PASSWORD: str = os.getenv("EMAIL_HOST_PASSWORD", "test_password")
+    EMAIL_USE_TLS: bool = os.getenv("EMAIL_USE_TLS", "False").lower() == "true"
+    MAILHOG_API_PORT: int = os.getenv("MAILHOG_API_PORT", 8025)
+
 
 class Settings(BaseAppSettings):
     POSTGRES_USER: str = os.getenv("POSTGRES_USER", "test_user")
