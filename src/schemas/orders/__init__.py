762edb8598c7963fe46942c56910eac4a2cad5ff
SensibleN00B <sensible.noob@gmail.com>
2025-10-15 16:50:29 +0300

    feature: add schemas for orders, including request and response models

---
+++ src/schemas/orders/__init__.py
@@ -0,0 +1,13 @@
+from .orders import (
+    OrderCreateRequestSchema,
+    OrderItemResponseSchema,
+    OrderPaginatedResponseSchema,
+    OrderResponseSchema,
+)
+
+__all__ = [
+    "OrderCreateRequestSchema",
+    "OrderItemResponseSchema",
+    "OrderResponseSchema",
+    "OrderPaginatedResponseSchema",
+]
