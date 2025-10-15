8b3670a655cc93a036f286a3340b14d176f1d7f4
Oleksandr Popov <orbik.rumion22@gmail.com>
2025-10-16 00:47:52 +0300

    feature: connect favorite router to the main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from routes import cart_router, movie_router, order_router, payment_router
+from routes import cart_router, movie_router, order_router, payment_router, favorites_router
 from routes.accounts import router as accounts_router
 from routes.profiles import router as profiles_router
 
@@ -19,4 +19,5 @@ app.include_router(order_router, prefix=f"{api_version_prefix}", tags=["orders"]
 app.include_router(payment_router, prefix=f"{api_version_prefix}", tags=["payments"])
 app.include_router(accounts_router, prefix=f"{api_version_prefix}/accounts", tags=["accounts"])
 app.include_router(profiles_router, prefix=f"{api_version_prefix}/profiles", tags=["profiles"])
-app.include_router(cart_router, prefix=f"{api_version_prefix}", tags=[""])
+app.include_router(cart_router, prefix=f"{api_version_prefix}", tags=["cart"]),
+app.include_router(favorites_router, prefix=f"{api_version_prefix}", tags=["favorites"])
