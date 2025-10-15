b37573c0df27d8cf0d188893c3de894133c5e43f
MorsImmortalis05 <morsimmortalis05@gmail.com>
2025-10-15 11:13:54 +0300

    removed migrations

--- src/database/migrations/versions/e1914cbaac51_create_cart_table.py
+++ src/database/migrations/versions/e1914cbaac51_create_cart_table.py
@@ -11,11 +11,8 @@ from typing import Sequence, Union
 import sqlalchemy as sa
 from alembic import op
 
-<<<<<<< HEAD
 import database
 
-=======
->>>>>>> c765b12 (fixed formatting)
 # revision identifiers, used by Alembic.
 revision: str = "e1914cbaac51"
 down_revision: Union[str, Sequence[str], None] = "07953b0dfbff"
--- src/database/migrations/versions/eb47f63ffdc0_temp_migration.py
+++ src/database/migrations/versions/eb47f63ffdc0_temp_migration.py
@@ -11,11 +11,8 @@ from typing import Sequence, Union
 import sqlalchemy as sa
 from alembic import op
 
-<<<<<<< HEAD
 import database
 
-=======
->>>>>>> c765b12 (fixed formatting)
 # revision identifiers, used by Alembic.
 revision: str = "eb47f63ffdc0"
 down_revision: Union[str, Sequence[str], None] = "e1914cbaac51"
