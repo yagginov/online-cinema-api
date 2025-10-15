94bc67a92806124a2275c1fcb36b7a7f92c624ba
Oleksandr Popov <orbik.rumion22@gmail.com>
2025-10-16 00:53:36 +0300

    refactor: fixed mypy problem

--- src/main.py
+++ src/main.py
@@ -19,5 +19,5 @@ app.include_router(order_router, prefix=f"{api_version_prefix}", tags=["orders"]
 app.include_router(payment_router, prefix=f"{api_version_prefix}", tags=["payments"])
 app.include_router(accounts_router, prefix=f"{api_version_prefix}/accounts", tags=["accounts"])
 app.include_router(profiles_router, prefix=f"{api_version_prefix}/profiles", tags=["profiles"])
-app.include_router(cart_router, prefix=f"{api_version_prefix}", tags=["cart"]),
+app.include_router(cart_router, prefix=f"{api_version_prefix}", tags=["cart"])
 app.include_router(favorites_router, prefix=f"{api_version_prefix}", tags=["favorites"])
