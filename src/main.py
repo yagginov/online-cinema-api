1a0ca2b076961da98e8125c5ed5bbe937572e3bd
Bohdan Yagginov <bhders62@gmail.com>
2025-10-15 11:04:16 +0300

    refactor: add additional blank line after test endpoint

--- src/main.py
+++ src/main.py
@@ -13,6 +13,7 @@ api_version_prefix = "/api/v1"
 async def get_index():
     return {"message": "Hello World"}
 
+
 app.include_router(movie_router, prefix=f"{api_version_prefix}/cinema", tags=["cinema"])
 app.include_router(accounts_router, prefix=f"{api_version_prefix}/accounts", tags=["accounts"])
 app.include_router(profiles_router, prefix=f"{api_version_prefix}/profiles", tags=["profiles"])
