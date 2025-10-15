3b864223ca7785290d92a4c8567e4ddde955ff4b
Oleksandr Popov <orbik.rumion22@gmail.com>
2025-10-16 00:47:25 +0300

    feature: created endpoints for favorites

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -1,4 +1,5 @@
 from .cart import router as cart_router
+from .favorites import router as favorites_router
 from .movies import router as movie_router
 from .orders import router as order_router
 from .payments import router as payment_router
