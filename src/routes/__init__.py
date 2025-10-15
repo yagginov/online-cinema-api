cd650f456a3acd1442ad2675514cec6c50e63900
SensibleN00B <sensible.noob@gmail.com>
2025-10-15 23:25:19 +0300

    feature: add `payments` endpoints for payment processing and integrate `payment_router` into the application
    # Conflicts:
    #	src/main.py

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -2,3 +2,4 @@ from .cart import router as cart_router
 from .favorites import router as favorites_router
 from .movies import router as movie_router
 from .orders import router as order_router
+from .payments import router as payment_router
